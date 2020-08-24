e8495b0dc24f2f0c3daa9fd386475a10190fed82
darrylgit <darrylsmithup@gmail.com>
2020-08-24 09:51:32 -0400

    Create comparePassword method

--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,39 @@ userSchema.pre('save', function () {
   if (!user.isModified('password')) {
     return next();
   }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
 
 mongoose.model('User', userSchema);
