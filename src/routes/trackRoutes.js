7edf9e1e59ac69147fd60c88b5bd7ab9d3e8ac75
darrylgit <darrylsmithup@gmail.com>
2020-08-24 14:39:05 -0400

    Create POST request for tracks

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -14,4 +14,23 @@ router.get('/tracks', async (req, res) => {
   res.send(tracks);
 });
 
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body;
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations.' });
+  }
+
+  const track = new Track({ name, locations, userId: req.user._id });
+
+  try {
+    await track.save();
+    res.send(track);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
 module.exports = router;
