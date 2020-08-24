26dac1a1c290a7822ad899e926bc01d9aa074fcc
darrylgit <darrylsmithup@gmail.com>
2020-08-24 09:58:43 -0400

    Create sign in route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,4 +19,26 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
+});
+
 module.exports = router;
